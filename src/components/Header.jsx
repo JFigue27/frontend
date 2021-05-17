1706b022ab6df5e6e77948e612162e586ed1f79b
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2021-05-16 22:20:31 -0600

    Working with styles.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../styles/components/Header.styl';
 
 const Header = ({ children }) => {
   return (
     <div>
-      <h1 className="Header-title">Header</h1>
+      <div className="Header-title">
+        <div className="Avatar">
+          <img src="../styles/images/jjfigueroa.jpg" alt="Jaime Figueroa" />
+        </div>
+        <h1>Jose Jaime Figueroa Barrios</h1>
+      </div>
       {children}
     </div>
   );
